fd92fc95c19d1df8c73bd218fe605fc7773299bf
Md Nasim Hosen <mdnasimhosen333@gmail.com>
2024-04-19 00:51:23 +0600

    add something in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,24 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import config from "./app/config";
+import globalErrorHandler from "./app/middlewares/globalErrorHandler";
+import notFount from "./app/middlewares/notFound";
 const app: Application = express();
 
 // parsers
 app.use(express.json());
 app.use(cors());
 
+// app.use("/api", router);
+
 app.get("/", (req: Request, res: Response) => {
   res.send(`Server Running on port ${config.port}`);
 });
+
+// global error handler middleware
+app.use(globalErrorHandler);
+
+// not found middleware
+app.use(notFount);
+
 export default app;
