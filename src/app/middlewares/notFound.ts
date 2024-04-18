fbf81464e1634da7dadcd749fbc4108837bdd773
Md Nasim Hosen <mdnasimhosen333@gmail.com>
2024-04-19 01:08:50 +0600

    some chnage

--- src/app/middlewares/notFound.ts
+++ src/app/middlewares/notFound.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status";
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars, no-unused-vars
 const notFount = (req: Request, res: Response, next: NextFunction) => {
   res.status(httpStatus.NOT_FOUND).json({
     success: false,
