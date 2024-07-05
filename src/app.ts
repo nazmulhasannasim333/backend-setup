1e519165a194fdded9c0b323f06dd8ddeea8ad0d
Md Nasim Hosen <mdnasimhosen333@gmail.com>
2024-07-05 23:33:05 +0600

    some issue fixed

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,12 @@ const app: Application = express();
 
 // parsers
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 
 // app.use("/api", router);
 
